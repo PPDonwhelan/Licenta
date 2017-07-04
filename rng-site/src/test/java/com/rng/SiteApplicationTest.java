466e1d3f0e45c543304853bff25b9bf922cebf79
moldovanm2 <Maria.Moldovan@paddypowerbetfair.com>
2017-07-04 15:37:40 +0300

    Correct -stable version

---
+++ rng-site/src/test/java/com/rng/SiteApplicationTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.rng;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+/**
+ * @author Siva
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SiteApplication.class)
+public class SiteApplicationTest
+{
+	@Test
+	public void testDummy()
+	{
+	}
+	
+}
